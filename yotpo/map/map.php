ac35da9e656cf65107a78122cac5dea63fe5d02e
Alon Braitstein <alon@yotpo.com>
2013-01-01 17:55:34 +0200

    initial commit of plugin

---
+++ yotpo/map/map.php
@@ -0,0 +1,88 @@
+<?php
+class Map 
+{
+  const YOTPO_OAUTH_TOKEN_URL = "https://api.yotpo.com/oauth/token";
+  const YOTPO_API_URL = "https://api.yotpo.com/apps";
+  const HTTP_REQUEST_TIMEOUT = 30;
+
+
+  public static function mailAfterPurchase($params, $context)
+  {
+
+    $app_key = Configuration::get($context->name.'_app_key');
+    $secret = Configuration::get($context->name.'_oauth_token');
+    $enable_feature = Configuration::get($context->name.'_map_enabled');
+    
+    //check if both app_key and secret exist
+    if(($app_key == null) or ($secret == null) or $enable_feature == "0" || $enable_feature == NULL)
+    {
+      return;
+    }
+    
+    $OAuthStorePath = _PS_ROOT_DIR_ . _MODULE_DIR_ . $context->name . DS . 'lib'. DS .'oauth-php' . DS . 'library' . DS . 'OAuthStore.php';
+    $OAuthRequesterPath = _PS_ROOT_DIR_ .  _MODULE_DIR_ . $context->name . DS . 'lib'. DS .'oauth-php' . DS . 'library' . DS . 'OAuthRequester.php';
+
+    require_once ($OAuthStorePath);
+    require_once ($OAuthRequesterPath);
+
+    $data = array();
+
+    $customer = new Customer((int)$params['cart']->id_customer);
+
+    $data["email"] = $customer->email;
+    $data["customer_name"] = $customer->firstname . ' ' . $customer->lastname;
+    $data["order_id"] = $params['id_order'];
+    $data['platform'] = 'prestashop';
+
+    $products = OrderDetail::getList($params['id_order']);
+    $products_arr = array();
+
+    foreach ($products as $product) {
+
+      $product_data = array();
+      
+      $full_product = new Product((int)($product['product_id']), false, (int)($params['cookie']->id_lang));      
+      $product_data['url'] = $full_product->getLink();  
+      $product_data['name'] = $full_product->name;
+      $product_data['image'] = $context->_getProductImageUrl($product['product_id']);
+      $product_data['description'] = strip_tags(nl2br($full_product->description));
+
+      $products_arr[$product['product_id']] = $product_data;
+    }
+
+    $data['products'] = $products_arr;
+    
+    $yotpo_options = array( 'consumer_key' => $app_key, 'consumer_secret' => $secret, 'client_id' => $app_key, 'client_secret' => $secret, 'grant_type' => 'client_credentials' );
+    
+    OAuthStore::instance("2Leg", $yotpo_options);
+
+    try
+    {
+
+      $request = new OAuthRequester(self::YOTPO_OAUTH_TOKEN_URL, "POST", $yotpo_options);         
+      $result = $request->doRequest(0);
+
+      $response = $result['body'];
+      $tokenParams = json_decode($result['body'], true);
+      $data['utoken'] = $tokenParams['access_token'];
+      $parsed_data = http_build_query($data);
+      $opts = array(
+        'http'=>array(
+          'method'=>"POST",
+          'timeout' => self::HTTP_REQUEST_TIMEOUT,
+          'header'=> "Content-type: application/x-www-form-urlencoded\r\n" .
+                     "Content-Length: " . strlen($parsed_data) . "\r\n",
+          'content' => $parsed_data
+        )
+      );
+      $feed_url = self::YOTPO_API_URL . DS . $app_key . "/purchases/";
+      $stream_context = @stream_context_create($opts); 
+      $fp = fopen($feed_url, 'r', false, $stream_context);
+    }
+    catch(OAuthException2 $e)
+    {
+      throw new Exception($e);   
+    }
+  }
+}
+?>
---
+++ yotpo/yotpo.php
@@ -0,0 +1,250 @@
+<?php
+if (!defined('_PS_VERSION_'))
+  exit;
+ 
+class Yotpo extends Module
+{
+
+  private $_html = '';
+  private $_postErrors = array();
+  public function __construct()
+    {
+      // version test
+      $version_mask = explode('.', _PS_VERSION_, 3);
+      $version_test = $version_mask[0] > 0 && $version_mask[1] > 3;
+
+      $this->name = 'yotpo';
+      $this->tab = $version_test ? 'advertising_marketing' : 'Yotpo';
+      $this->version = 1.0;
+      if($version_test)
+        $this->author = 'Alon';
+      $this->need_instance = 1;
+ 
+      parent::__construct();
+   
+      $this->displayName = $this->l('Yotpo');
+      $this->description = $this->l('Allow MAP');
+
+      require(_PS_MODULE_DIR_.'yotpo/map/map.php');
+    }
+ 
+  public function install()
+  {
+    //TODO make the second or part to be valid only if map check box is checked.
+    if (parent::install() == false OR !$this->registerHook('displayFooterProduct') 
+                                   OR !$this->registerHook('actionPaymentConfirmation')) {
+      return false;
+    }
+    return true;
+  }
+
+  public function hookdisplayFooterProduct($params)
+  {
+
+    global $smarty;
+    $product = $params['product'];
+// $this->logConsole('product   ====  ', json_encode($product->upc), false);
+    $smarty->assign('yotpoAppkey', Configuration::get($this->name.'_app_key'));
+    $smarty->assign('yotpoProductId', $product->id);
+    $smarty->assign('yotpoProductName', strip_tags(nl2br($product->name)));
+    $smarty->assign('yotpoProductDescription', strip_tags(nl2br($product->description)));
+    $smarty->assign('yotpoProductImageUrl', $this->_getProductImageUrl($product->id));
+    $smarty->assign('yotpoDomain', Tools::getShopDomain(false,false));
+    $smarty->assign('yotpoProductModel', $this->_getProductModel($product));
+    $smarty->assign('yotpoProductBreadCrumbs', $this->_getBreadCrumbs($product));
+    
+    // TODO check if can insert this in header part so it will be loaded only once
+    echo "<script src ='http://www.yotpo.com/js/yQuery.js'></script>";
+    return $this->display(__FILE__,'yotpo.tpl');
+  }
+
+  public function hookActionPaymentConfirmation($params)
+  {
+    Map::mailAfterPurchase($params, $this);
+  }
+
+  public function _getProductImageUrl($id_product)
+  {
+    $id_image = Product::getCover($id_product);
+    // get Image by id
+    if (sizeof($id_image) > 0) {
+        $image = new Image($id_image['id_image']);
+        // get image full URL
+        return $image_url = _PS_BASE_URL_._THEME_PROD_DIR_.$image->getExistingImgPath().".jpg";
+    }  
+    return NULL;
+  }
+
+  public function uninstall()
+  {
+    if (!parent::uninstall())
+      Db::getInstance()->Execute('DELETE FROM `'._DB_PREFIX_.'yotpo`');
+    parent::uninstall();
+  }
+
+// module configuration
+  public function getContent()
+  {
+    if (Tools::isSubmit('submit'))
+    {
+      $this->_postValidation();
+      if (!sizeof($this->_postErrors))
+      {
+       $this->_postProcess();
+      }
+      else
+      {
+        foreach ($this->_postErrors AS $err)
+        {
+          $this->_html .= '<div class="alert error">'.$err.'</div>';
+        }
+      }
+    }
+    $this->_displayForm();
+    return $this->_html;
+  }
+
+// module settings
+  private function _displayForm()
+  {
+    $yotpo_map_enabled = Configuration::get($this->name.'_map_enabled') == "0" ? false : true;
+    
+    $this->_html .= '<h2>'.$this->displayName.'</h2>';
+    $this->_html .= '
+    <form action="'.$_SERVER['REQUEST_URI'].'" method="post">
+      <fieldset>
+        <legend><img src="../img/admin/cog.gif" alt="" class="middle" />'.$this->l('Settings').'</legend>
+        <label>'.$this->l('App key').'</label>
+        <div class="margin-form">
+          <input type="text" name="yotpo_app_key" value="'.Tools::getValue('yotpo_app_key', Configuration::get($this->name.'_app_key')).'"/>
+        </div>
+        <label>'.$this->l('Secret token').'</label>
+        <div class="margin-form">
+          <input type="text" name="yotpo_oauth_token" value="'.Tools::getValue('yotpo_oauth_token', Configuration::get($this->name.'_oauth_token')).'"/>
+        </div>
+
+        <div class="margin-form">
+          <input type="checkbox" name="yotpo_map_enabled" value="yotpo_map_enabled" ';
+          if($yotpo_map_enabled == true)
+            $this->_html .= 'checked=checked';
+          $this->_html .= ''.$this->l('Mail after purchase'). '<br>
+        </div>
+        <input type="submit" name="submit" value="'.$this->l('Update').'" class="button" />
+      </fieldset>
+    </form>';
+  }
+
+  private function _postValidation()
+  {
+    $api_key = Tools::getValue('yotpo_app_key');
+    $secret_token = Tools::getValue('yotpo_oauth_token');
+    $map_enabled = Tools::getValue('yotpo_map_enabled');
+    if($api_key == '')
+      $this->_postErrors[] = $this->l('Please fill out the api key');
+    if($map_enabled && $secret_token == '')
+      $this->_postErrors[] = $this->l('Please fill out the secret token');      
+  }
+
+  private function _postProcess()
+  {
+    $yotpo_map_enabled = Tools::getValue('yotpo_map_enabled') == false ? "0" : "1";
+
+    Configuration::updateValue($this->name.'_map_enabled', $yotpo_map_enabled, false);
+    Configuration::updateValue($this->name.'_app_key', Tools::getValue('yotpo_app_key'), false);
+    Configuration::updateValue($this->name.'_oauth_token', Tools::getValue('yotpo_oauth_token'), false);
+
+    $this->_html .= '<div class="conf confirm">'.$this->l('Settings updated').'</div>';
+  }
+
+  private function _getProductModel($product)
+  {    
+    if(Validate::isEan13($product->ean13))
+    {
+      return $product->ean13;
+    }
+    else if(Validate::isUpc($product->upc))
+    {
+      return $product->upc;  
+    }
+    return NULL;
+  }
+
+  private function _getBreadCrumbs($product)
+  {
+   $result = '';
+   $all_product_subs = Product::getProductCategoriesFull($product->id, $this->context->language->id);
+   $all_product_subs_path = array();
+   if(isset($all_product_subs) && count($all_product_subs)>0)
+   {
+      foreach($all_product_subs as $subcat)
+      {
+        $sub_category = new Category($subcat['id_category'], $this->context->language->id);
+        $sub_category_path = $sub_category->getParentsCategories();
+        foreach ($sub_category_path as $key) {
+          $result .= ''.$key['name'].';';  
+        }
+        $result .= ',';  
+      }
+   }
+   if($result[strlen($result)-1] == ',')
+   {
+     $result = substr_replace($result ,"",-1); 
+   }
+   return $result;
+  }
+
+    /**
+     * Logs messages/variables/data to browser console from within php
+     *
+     * @param $name: message to be shown for optional data/vars
+     * @param $data: variable (scalar/mixed) arrays/objects, etc to be logged
+     * @param $jsEval: whether to apply JS eval() to arrays/objects
+     *
+     * @return none
+     * @author Sarfraz
+     */
+     function logConsole($name, $data = NULL, $jsEval = FALSE)
+     {
+          if (! $name) return false;
+
+          $isevaled = false;
+          $type = ($data || gettype($data)) ? 'Type: ' . gettype($data) : '';
+
+          if ($jsEval && (is_array($data) || is_object($data)))
+          {
+               $data = 'eval(' . preg_replace('#[\s\r\n\t\0\x0B]+#', '', json_encode($data)) . ')';
+               $isevaled = true;
+          }
+          else
+          {
+               $data = json_encode($data);
+          }
+
+          # sanitalize
+          $data = $data ? $data : '';
+          $search_array = array("#'#", '#""#', "#''#", "#\n#", "#\r\n#");
+          $replace_array = array('"', '', '', '\\n', '\\n');
+          $data = preg_replace($search_array,  $replace_array, $data);
+          $data = ltrim(rtrim($data, '"'), '"');
+          $data = $isevaled ? $data : ($data[0] === "'") ? $data : "'" . $data . "'";
+
+$js = <<<JSCODE
+\n<script>
+     // fallback - to deal with IE (or browsers that don't have console)
+     if (! window.console) console = {};
+     console.log = console.log || function(name, data){};
+     // end of fallback
+
+     console.log('$name');
+     console.log('------------------------------------------');
+     console.log('$type');
+     console.log($data);
+     console.log('\\n');
+</script>
+JSCODE;
+
+          echo $js;
+     } # end logConsole
+
+}
+?>
