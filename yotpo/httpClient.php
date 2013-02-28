b1a9d54c4cb55608eed1d11553fb52c94fae910c
Alon Braitstein <alon@yotpo.com>
2013-02-28 11:44:16 +0200

    Add support to past orders. Remove MAP checkbox.

--- yotpo/httpClient.php
+++ yotpo/httpClient.php
@@ -52,6 +52,16 @@ class YotpoHttpClient
 		return $token;
 	}
 
+	public function makePastOrdersRequest($data, $app_key, $secret_token)
+	{
+		$token = $this->grantOauthAccess($app_key, $secret_token);
+		if(isset($token))
+		{
+			$data['utoken'] = $token;
+		    $this->makePostRequest(self::YOTPO_API_URL . '/apps/' . $app_key . "/purchases/mass_create", $data);
+		}	
+	}
+
 	public function makeMapRequest($data, $app_key, $secret_token)
 	{
 		$token = $this->grantOauthAccess($app_key, $secret_token);
@@ -85,9 +95,8 @@ class YotpoHttpClient
 
 	private function grantOauthAccess($app_key, $secret_token)
 	{
-	   	$yotpo_dir_module = Configuration::get('yotpo_dir_module');
-    	$OAuthStorePath = $yotpo_dir_module .'/lib/oauth-php/library/OAuthStore.php';
-    	$OAuthRequesterPath = $yotpo_dir_module . '/lib/oauth-php/library/OAuthRequester.php';
+    	$OAuthStorePath = _PS_MODULE_DIR_ . 'yotpo/lib/oauth-php/library/OAuthStore.php';
+    	$OAuthRequesterPath = _PS_MODULE_DIR_ . 'yotpo/lib/oauth-php/library/OAuthRequester.php';
 		include_once ($OAuthStorePath);
 		include_once ($OAuthRequesterPath);	    		   	
 	    $yotpo_options = array( 'consumer_key' => $app_key, 'consumer_secret' => $secret_token, 'client_id' => $app_key, 'client_secret' => $secret_token, 'grant_type' => 'client_credentials' );
