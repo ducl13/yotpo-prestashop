b23596c267ff6ce476efa9490d8bc3e2bcd64f38
yoelb <yoel@yotpo.com>
2014-06-08 15:24:08 +0300

    added support for ie8 in event listener

--- yotpo/js/headerScript.js
+++ yotpo/js/headerScript.js
@@ -10,8 +10,14 @@ var inIframe = inIframe();
 if (inIframe) {
 	window['yotpo_testimonials_active'] = true;
 }
-
- document.addEventListener('DOMContentLoaded', function () {
+if (document.addEventListener){
+    document.addEventListener('DOMContentLoaded', function () {
         var e=document.createElement("script");e.type="text/javascript",e.async=true,e.src="//staticw2.yotpo.com/" + yotpoAppkey  + "/widget.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)
- });
+    });
+}
+else if (document.attachEvent) {
+    document.attachEvent('DOMContentLoaded',function(){
+        var e=document.createElement("script");e.type="text/javascript",e.async=true,e.src="//staticw2.yotpo.com/" + yotpoAppkey  + "/widget.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)
+    });
+}
   
